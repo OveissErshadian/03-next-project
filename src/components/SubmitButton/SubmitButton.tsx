00f1d5264c9d5efdad703d306b473c5a0726a750
Oveiss <oveiss.ershadian@gmail.com>
2025-09-18 12:25:11 +0330

    .

--- src/components/SubmitButton/SubmitButton.tsx
+++ src/components/SubmitButton/SubmitButton.tsx
@@ -1,10 +1,14 @@
-'use client'
+"use client";
 import { useFormStatus } from "react-dom";
 
 const SubmitButton = () => {
-  const {pending} = useFormStatus();
+  const { pending } = useFormStatus();
   return (
-    <button type="submit" disabled={pending} className="bg-red-500 p-4 rounded-2xl cursor-pointer disabled:bg-amber-300 disabled:opacity-60">
+    <button
+      type="submit"
+      disabled={pending}
+      className="bg-red-500 p-4 rounded-2xl cursor-pointer disabled:bg-amber-300 disabled:opacity-60"
+    >
       {pending ? "booking..." : "book event"}
     </button>
   );
